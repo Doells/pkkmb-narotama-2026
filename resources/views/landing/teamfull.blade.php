eb72d866775e8e89acb5d6bf019cbb4a65727fe3
Doells <irsyadr181@gmail.com>
2026-08-27 09:48:19 +0700

    update foto panitia

--- resources/views/landing/teamfull.blade.php
+++ resources/views/landing/teamfull.blade.php
@@ -3,51 +3,51 @@
 @section('content')
 @php
 $teams = [
-    ['name' => 'Ade Chandra Kurnia Purwanto, S.Kom., M.H.', 'title' => 'Kepala Sub Direktorat Kemahasiswaan, Alumni, Tracer Study', 'link' => 'https://www.instagram.com/masss.chan/'],
-    ['name' => 'Amalia Prastika Sari, S.Pd.', 'title' => 'Staf Direktorat Kemahasiswaan, Alumni, Tracer Study', 'link' => 'https://www.instagram.com/amaliaprastika/'],
-    ['name' => 'Alfandi Kusuma', 'title' => 'Steering Commite', 'link' => 'https://www.instagram.com/alfama.id/'],
-    ['name' => 'Nilasari Eka Ambarwati', 'title' => 'Steering Commite', 'link' => 'https://www.instagram.com/_vanniillaa/'],
-    ['name' => 'Toti Valentino Putra', 'title' => 'Steering Commite', 'link' => 'https://www.instagram.com/tvaltra_/'],
-    ['name' => 'Mochammad Dicky Tri Pranata', 'title' => 'Ketua Pelaksana', 'link' => 'https://www.instagram.com/dikianaksingkong/'],
-    ['name' => 'Rina Nur Aminah', 'title' => 'Koordinator Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/rinaaja_28/'],
-    ['name' => 'Siti Masriyah', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/xriyahh._/'],
-    ['name' => 'Chyntia Hasde', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/_chyfss/'],
-    ['name' => 'Meyla Shafa', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/meyyshafaaa/'],
-    ['name' => 'Tiara Atikah Almasah', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/tiaraatikahh/'],
-    ['name' => 'Meltina Julianti Rumheng', 'title' => 'Koordinator Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/meltinarumheng/'],
-    ['name' => 'Annisa Nurul Rachma', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/ansxxnr/'],
-    ['name' => 'Faiz Rasya Nastain', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/nxstaaa.r/'],
-    ['name' => 'Ninda Aprilia Rochma', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/nindaaprl_/'],
-    ['name' => 'Sabrina Ayu Anggraeni', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/__heyitsbina/'],
-    ['name' => 'Nadia Octovani Adelia', 'title' => 'Koordinator Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/octonaaa_/'],
-    ['name' => 'Muhammad Reza A', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/rezafifn/'],
-    ['name' => 'Nuriza Yudista Sari', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/nr.ydistasr/'],
-    ['name' => 'Ainun Rahmadhani', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/ainunrmdn_/'],
-    ['name' => 'Fredo Rizky Dewantara', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/rizkyy_dewantaraa/'],
-    ['name' => 'Pinkan Wardah Aulia', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/pnkwra/'],
-    ['name' => 'Maulana Adrian Saputra', 'title' => 'Koordinator Divisi Perlengkapan', 'link' => 'https://www.instagram.com/adriaansapp/'],
-    ['name' => 'Achmad Farros Robihansyah', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/faizz.yh_/'],
-    ['name' => 'Muhamad Haris Al-Aziz', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/haris_alazis/'],
-    ['name' => 'Lina Sefiyatiningsih', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/buihkentut/'],
-    ['name' => 'Muhammad Sofi', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/_mhmmdsofi/'],
-    ['name' => 'Wida Humairoh Primarani', 'title' => 'Koordinator Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/wdaprm_/'],
-    ['name' => 'Martina Rahael', 'title' => 'Anggota Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/vy_rehell/'],
-    ['name' => 'Jayanti Nur Fatika', 'title' => 'Anggota Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/atikajy/'],
-    ['name' => 'Zainal Abidin', 'title' => 'Anggota Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/zainsva_/'],
-    ['name' => 'Benedictus Berttria Octo Raharjo', 'title' => 'Koordinator Divisi Naradamping', 'link' => 'https://www.instagram.com/albeneex/'],
-    ['name' => 'Moh. Ruki Ardiansyah', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/rukiardnsyh/'],
-    ['name' => 'Rivky Dimas Ariyanto', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/rivdimz_/'],
-    ['name' => 'Andien Putri Oveanti', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/_andienputri/'],
-    ['name' => 'Merlin Rumthe', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/uknowmer/'],
-    ['name' => 'Afni Rahma Andini', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/afnra9/'],
-    ['name' => 'Rohma Fitriani', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/_rhmafitrni/'],
-    ['name' => 'Intan Permata Sari Sinaga', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/intaan_0.0/'],
-    ['name' => 'Karina Vellishia', 'title' => 'Koordinator Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/karinaaaav_/'],
-    ['name' => 'Riska Ayu Purnamasari', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/gizso/'],
-    ['name' => 'Maulana Ahmad Afandi', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/innsencc_/'],
-    ['name' => 'Fanny Leonora Lekito', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/fannylekitoo_/'],
-    ['name' => 'Herlin Diana', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/ilenbily/'],
-    ['name' => 'Nabilah Dwi Ramadhani', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/_nbilahdwi/'],
+    ['name' => 'Ade Chandra Kurnia Purwanto, S.Kom., M.H.', 'image' => 'img/panitia/ade.jpg', 'title' => 'Kepala Sub Direktorat Kemahasiswaan, Alumni, Tracer Study', 'link' => 'https://www.instagram.com/masss.chan/'],
+    ['name' => 'Amalia Prastika Sari, S.Pd.', 'image' => 'img/panitia/Amalia Prastika Sari, S.Pd.jpg', 'title' => 'Staf Direktorat Kemahasiswaan, Alumni, Tracer Study', 'link' => 'https://www.instagram.com/amaliaprastika/'],
+    ['name' => 'Alfandi Kusuma', 'image' => 'img/panitia/Alfandi Kusuma.jpg', 'title' => 'Steering Commite', 'link' => 'https://www.instagram.com/alfama.id/'],
+    ['name' => 'Nilasari Eka Ambarwati', 'image' => 'img/panitia/Nilasari Eka Ambarwati.jpg', 'title' => 'Steering Commite', 'link' => 'https://www.instagram.com/_vanniillaa/'],
+    ['name' => 'Toti Valentino Putra', 'image' => 'img/panitia/Toti Valentino Putra.jpg', 'title' => 'Steering Commite', 'link' => 'https://www.instagram.com/tvaltra_/'],
+    ['name' => 'Mochammad Dicky Tri Pranata', 'image' => 'img/panitia/Moch. Dicky Tri Pranata.jpg', 'title' => 'Ketua Pelaksana', 'link' => 'https://www.instagram.com/dikianaksingkong/'],
+    ['name' => 'Rina Nur Aminah', 'image' => 'img/panitia/Rina Nur Aminah.jpg', 'title' => 'Koordinator Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/rinaaja_28/'],
+    ['name' => 'Siti Masriyah', 'image' => 'img/panitia/Siti Masriyah.jpg', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/xriyahh._/'],
+    ['name' => 'Chyntia Hasde', 'image' => 'img/panitia/Chyntia Hasde.jpg', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/_chyfss/'],
+    ['name' => 'Meyla Shafa', 'image' => 'img/panitia/Meyla Shafa Rizkianti.jpg', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/meyyshafaaa/'],
+    ['name' => 'Tiara Atikah Almasah', 'image' => 'img/panitia/Tiara Atikah Almasah.jpg', 'title' => 'Anggota Divisi Kesekretariatan', 'link' => 'https://www.instagram.com/tiaraatikahh/'],
+    ['name' => 'Meltina Julianti Rumheng', 'image' => 'img/panitia/Meltina Julianti Rumheng.jpg', 'title' => 'Koordinator Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/meltinarumheng/'],
+    ['name' => 'Annisa Nurul Rachma', 'image' => 'img/panitia/Anisa Nurul Rachma.jpg', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/ansxxnr/'],
+    ['name' => 'Faiz Rasya Nastain', 'image' => 'img/panitia/Faiz Rasya Nastain.jpg', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/nxstaaa.r/'],
+    ['name' => 'Ninda Aprilia Rochma', 'image' => 'img/panitia/Ninda Aprilia Rochma.jpg', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/nindaaprl_/'],
+    ['name' => 'Sabrina Ayu Anggraeni', 'image' => 'img/panitia/Sabrina Ayu Anggraeni.jpg', 'title' => 'Anggota Divisi Acara dan Kreatif', 'link' => 'https://www.instagram.com/__heyitsbina/'],
+    ['name' => 'Nadia Octovani Adelia', 'image' => 'img/panitia/Nadia Octovani Adelia.jpg', 'title' => 'Koordinator Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/octonaaa_/'],
+    ['name' => 'Muhammad Reza A', 'image' => 'img/panitia/Muhamad Reza Afifudin.jpg', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/rezafifn/'],
+    ['name' => 'Nuriza Yudista Sari', 'image' => 'img/panitia/Nuriza Yudista Sari.jpg', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/nr.ydistasr/'],
+    ['name' => 'Ainun Rahmadhani', 'image' => 'img/panitia/Ainun Rahmadhani.jpg', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/ainunrmdn_/'],
+    ['name' => 'Fredo Rizky Dewantara', 'image' => 'img/panitia/Fredo Rizky Dewantara.jpg', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/rizkyy_dewantaraa/'],
+    ['name' => 'Pinkan Wardah Aulia', 'image' => 'img/panitia/Pinkan Wardah Aulia.jpg', 'title' => 'Anggota Divisi Dokumentasi & Teknologi Informasi', 'link' => 'https://www.instagram.com/pnkwra/'],
+    ['name' => 'Maulana Adrian Saputra', 'image' => 'img/panitia/Maulana Andrian Saputra.jpg', 'title' => 'Koordinator Divisi Perlengkapan', 'link' => 'https://www.instagram.com/adriaansapp/'],
+    ['name' => 'Achmad Farros Robihansyah', 'image' => 'img/panitia/Achmad Farros Robihansyah.jpg', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/faizz.yh_/'],
+    ['name' => 'Muhamad Haris Al-Aziz', 'image' => 'img/panitia/Muhamad Haris Al-Aziz.jpg', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/haris_alazis/'],
+    ['name' => 'Lina Sefiyatiningsih', 'image' => 'img/panitia/Lina Sefiyatiningsih.jpg', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/buihkentut/'],
+    ['name' => 'Muhammad Sofi', 'image' => 'img/panitia/Muhammad Sofi.jpg', 'title' => 'Anggota Divisi Perlengkapan', 'link' => 'https://www.instagram.com/_mhmmdsofi/'],
+    ['name' => 'Wida Humairoh Primarani', 'image' => 'img/panitia/Wida Humairoh Primarani.jpg', 'title' => 'Koordinator Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/wdaprm_/'],
+    ['name' => 'Martina Rahael', 'image' => 'img/panitia/Martina Rahael.jpg', 'title' => 'Anggota Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/vy_rehell/'],
+    ['name' => 'Jayanti Nur Fatika', 'image' => 'img/panitia/Jayanti Nur Fatika.jpg', 'title' => 'Anggota Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/atikajy/'],
+    ['name' => 'Zainal Abidin', 'image' => 'img/panitia/Zainal Abidin.jpg', 'title' => 'Anggota Divisi Hubungan Masyarakat', 'link' => 'https://www.instagram.com/zainsva_/'],
+    ['name' => 'Benedictus Berttria Octo Raharjo', 'image' => 'img/panitia/Benedictus Berttria Octo Raharjo.jpg', 'title' => 'Koordinator Divisi Naradamping', 'link' => 'https://www.instagram.com/albeneex/'],
+    ['name' => 'Moh. Ruki Ardiansyah', 'image' => 'img/panitia/Moh. Ruki Ardiansyah.jpg', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/rukiardnsyh/'],
+    ['name' => 'Rivky Dimas Ariyanto', 'image' => 'img/panitia/Rivky Dimas Ariyanto.jpg', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/rivdimz_/'],
+    ['name' => 'Andien Putri Oveanti', 'image' => 'img/panitia/Andien Putri Oveanti.jpg', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/_andienputri/'],
+    ['name' => 'Merlin Rumthe', 'image' => 'img/panitia/Merlin Rumthe.jpg', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/uknowmer/'],
+    ['name' => 'Afni Rahma Andini', 'image' => 'img/panitia/Afni Rahma Andini.jpg', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/afnra9/'],
+    ['name' => 'Rohma Fitriani', 'image' => 'img/panitia/Rohma Fitriani.jpg', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/_rhmafitrni/'],
+    ['name' => 'Intan Permata Sari Sinaga', 'image' => 'img/panitia/Intan Permata Sari Sinaga.jpg', 'title' => 'Anggota Divisi Naradamping', 'link' => 'https://www.instagram.com/intaan_0.0/'],
+    ['name' => 'Karina Vellishia', 'image' => 'img/panitia/Karina Vellishia.jpg', 'title' => 'Koordinator Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/karinaaaav_/'],
+    ['name' => 'Riska Ayu Purnamasari', 'image' => 'img/panitia/Riska Ayu Purnamasari.jpg', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/gizso/'],
+    ['name' => 'Maulana Ahmad Afandi', 'image' => 'img/panitia/Maulana Ahmad Afandi.jpg', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/innsencc_/'],
+    ['name' => 'Fanny Leonora Lekito', 'image' => 'img/panitia/Fanny Leonora Lekito.jpg', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/fannylekitoo_/'],
+    ['name' => 'Herlin Diana', 'image' => 'img/panitia/Herlin Diana.jpg', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/ilenbily/'],
+    ['name' => 'Nabilah Dwi Ramadhani', 'image' => 'img/panitia/Nabilah Dwi Ramadhani.jpg', 'title' => 'Anggota Divisi Logistik dan Kesehatan', 'link' => 'https://www.instagram.com/_nbilahdwi/'],
 ];
 @endphp
 
@@ -74,8 +74,12 @@ $teams = [
                         $initial = strtoupper(substr($sanitized, 0, 1) ?: 'N');
                     @endphp
                     <div class="bg-white rounded-[16px] shadow-sm hover:shadow-md border border-cine-navy/10 flex flex-col items-center p-[20px] transition-all hover:border-cine-blue">
-                        <div class="w-[64px] h-[64px] bg-cine-navy/5 rounded-full flex flex-shrink-0 items-center justify-center mb-4 text-cine-navy transition-colors">
-                            <span class="text-2xl font-black">{{ $initial }}</span>
+                        <div class="w-[100px] h-[100px] bg-cine-navy/5 rounded-2xl flex flex-shrink-0 items-center justify-center mb-4 text-cine-navy transition-colors overflow-hidden border-2 border-transparent">
+                            @if(isset($person['image']))
+                                <img src="{{ asset($person['image']) }}" alt="{{ $person['name'] }}" class="w-full h-full object-cover">
+                            @else
+                                <span class="text-3xl font-black">{{ $initial }}</span>
+                            @endif
                         </div>
                         <div class="text-center flex-grow flex flex-col w-full">
                             <h1 class="font-bold text-cine-navy text-sm mb-1 leading-snug">{{ $person['name'] }}</h1>
