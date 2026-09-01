ed6d5b2e86b618e58d621808635a36c2cce27080
Doells <irsyadr181@gmail.com>
2026-09-01 21:24:48 +0700

    tambahan meta blade

--- resources/views/includes/landing/meta.blade.php
+++ resources/views/includes/landing/meta.blade.php
@@ -1,5 +1,15 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Langkah pertama menuju perjalanan baru. Temukan teman, pengalaman, dan semangat untuk menjadi bagian dari Garda Depan di PKKMB Universitas Narotama 2026.">
+<meta name="keywords" content="PKKMB Narotama, PKKMB 2026, Universitas Narotama, Mahasiswa Baru, Ospek">
+<meta name="author" content="Universitas Narotama">
+<meta name="robots" content="index, follow">
+
+<meta property="og:title" content="PKKMB Universitas Narotama 2026">
+<meta property="og:description" content="Langkah pertama menuju perjalanan baru. Temukan teman, pengalaman, dan semangat untuk menjadi bagian dari Garda Depan di PKKMB Universitas Narotama 2026.">
+<meta property="og:image" content="{{ asset('pkkmblogo-transparent.png') }}">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:type" content="website">
 
 <link rel="icon" type="image/png" href="{{ asset('pkkmblogo-transparent.png') }}?v={{ filemtime(public_path('pkkmblogo-transparent.png')) }}">
 <link rel="shortcut icon" type="image/png" href="{{ asset('pkkmblogo-transparent.png') }}?v={{ filemtime(public_path('pkkmblogo-transparent.png')) }}">
