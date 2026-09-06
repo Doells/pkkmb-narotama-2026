d473038dc9124a503512925a97edb3774e771dbc
Doells <irsyadr181@gmail.com>
2026-09-06 21:35:51 +0700

    update login

--- resources/views/includes/landing/navbarlanding.blade.php
+++ resources/views/includes/landing/navbarlanding.blade.php
@@ -34,7 +34,11 @@
                 <div class="hidden justify-center w-full md:flex md:w-auto md:order-1 items-center space-x-1">
                     <a href="{{ route('index-landing') }}" class="block {{ request()->is('/') ? 'pt-8 pb-1 px-2 text-white font-sans font-bold hover:text-white border-b-2 border-white hover:border-white transition duration-300' : 'pt-8 pb-1 px-2 text-white font-sans font-normal hover:text-gray-300 hover:border-b-2 hover:border-gray-300 transition duration-300' }}">Beranda</a>
                     <a href="{{ route('informasi-landing') }}" class="block {{ request()->is('informasi') ? 'pt-8 pb-1 px-2 text-white font-sans font-bold hover:text-white border-b-2 border-white hover:border-white transition duration-300' : 'pt-8 pb-1 px-2 text-white font-sans font-normal hover:text-gray-300 hover:border-b-2 hover:border-gray-300 transition duration-300' }}">Informasi</a>
-                    <a href="{{ route('home-presences.indexuserdashboard') }}" class="block {{ request()->is('login') ? 'pt-8 pb-1 px-2 text-white font-sans font-bold hover:text-white border-b-2 border-white hover:border-white transition duration-300' : 'pt-8 pb-1 px-2 text-white font-sans font-normal hover:text-gray-300 hover:border-b-2 hover:border-gray-300 transition duration-300' }}">Dashboard</a>
+                    @auth
+                        <a href="{{ route('home-presences.indexuserdashboard') }}" class="block {{ request()->is('login') ? 'pt-8 pb-1 px-2 text-white font-sans font-bold hover:text-white border-b-2 border-white hover:border-white transition duration-300' : 'pt-8 pb-1 px-2 text-white font-sans font-normal hover:text-gray-300 hover:border-b-2 hover:border-gray-300 transition duration-300' }}">Dashboard</a>
+                    @else
+                        <a href="https://peserta-pkkmb.narotama.ac.id/auth" class="block {{ request()->is('login') ? 'pt-8 pb-1 px-2 text-white font-sans font-bold hover:text-white border-b-2 border-white hover:border-white transition duration-300' : 'pt-8 pb-1 px-2 text-white font-sans font-normal hover:text-gray-300 hover:border-b-2 hover:border-gray-300 transition duration-300' }}">Login</a>
+                    @endauth
                 </div>
             </div>
 
@@ -46,7 +50,11 @@
         <ul>
             <li><a href="{{ route('index-landing') }}" class="block {{ request()->is('/') ? 'text-sm px-2 py-4 font-sans font-normal text-white bg-primary hover:bg-white transition duration-500' : 'text-sm px-2 py-4 font-sans font-normal text-gray-600 hover:text-white hover:bg-white transition duration-500' }}">Profil</a></li>
             <li><a href="{{ route('informasi-landing') }}" class="block {{ request()->is('informasi') ? 'text-sm px-2 py-4 font-sans font-normal text-white bg-primary hover:bg-white transition duration-500' : 'text-sm px-2 py-4 font-sans font-normal text-gray-600 hover:text-white hover:bg-white transition duration-500' }}">Pedoman</a></li>
-            <li><a href="{{ route('home-presences.indexuserdashboard') }}" class="block {{ request()->is('login') ? 'text-sm px-2 py-4 font-sans font-normal text-white bg-primary hover:bg-white transition duration-500' : 'text-sm px-2 py-4 font-sans font-normal text-gray-600 hover:text-white hover:bg-white transition duration-500' }}">Beasiswa</a></li>
+            @auth
+                <li><a href="{{ route('home-presences.indexuserdashboard') }}" class="block {{ request()->is('login') ? 'text-sm px-2 py-4 font-sans font-normal text-white bg-primary hover:bg-white transition duration-500' : 'text-sm px-2 py-4 font-sans font-normal text-gray-600 hover:text-white hover:bg-white transition duration-500' }}">Dashboard</a></li>
+            @else
+                <li><a href="https://peserta-pkkmb.narotama.ac.id/auth" class="block {{ request()->is('login') ? 'text-sm px-2 py-4 font-sans font-normal text-white bg-primary hover:bg-white transition duration-500' : 'text-sm px-2 py-4 font-sans font-normal text-gray-600 hover:text-white hover:bg-white transition duration-500' }}">Login</a></li>
+            @endauth
         </ul>
     </div>
 </nav>
