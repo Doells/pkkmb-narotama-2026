a3235d6bf4bda001459150b0062b0832d769abe8
Doells <irsyadr181@gmail.com>
2026-09-06 20:28:56 +0700

    redirect website ke aplikasi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,14 +23,8 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-
-            // Jika peserta (Role/Position ID 1 biasanya mahasiswa peserta pkkmb)
-            if (auth()->user()->position_id == 1) {
-                return redirect()->away('https://peserta-pkkmb.narotama.ac.id');
-            }
-
-            // Pesan sukses jika berhasil login (Admin/Panitia)
-            return redirect()->intended('/dashboard')->with('loginSuccess', 'Login berhasil! Silahkan masuk.');
+            
+            return redirect()->away('https://peserta-pkkmb.narotama.ac.id');
         }
 
         // Pesan error jika login gagal
